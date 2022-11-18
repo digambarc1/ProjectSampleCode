f0d568a3071fa3d4644466579147791aaa2193fe
Digambar Kamalkar Chaudhari <digambarc@cybage.com>
2022-11-18 15:56:57 +0530

    second file added

---
+++ interf2/interf2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace interf2
+{
+    public class Class1:Program
+    {
+        public void test()
+        {
+            Console.WriteLine("Test 1 called");
+        }
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            string name;
+            Console.WriteLine("student name is" + c.name);
+            name = Console.ReadLine();
+            Console.WriteLine("usernam" + name);
+
+            //Console.WriteLine("student city is" + c.city);
+            //Console.ReadLine();
+            //Console.WriteLine("city" + c.city);
+            //c.Student();
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ interf2/interf2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interf2
+{
+    public class Program
+    {
+        //fields
+       public string name;
+      public  string city;
+       public string Address;
+
+        public void Student()
+        {
+          
+            Console.ReadLine();
+            Console.WriteLine( name);
+            //Console.WriteLine("student City is " + city);
+            //Console.WriteLine("student Address is"+Address);
+        }
+       //public Program ()
+       // {
+       //     Console.WriteLine("Parameterless constructor invoked");
+       //     Console.ReadLine();
+       // }
+       
+    }
+}
